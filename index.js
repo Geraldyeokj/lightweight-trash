26bc8e2e1037524db6b6bcc6ac5e13c8e0e50daa
Geraldyeokj <geraldyeokj@gmail.com>
2023-08-30 07:22:59 -0500

    changed port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const scheduledFunctions = require('./scheduledFunctions.js');
 const app = express();
-app.set("port", process.env.PORT || 7000);
+app.set("port", process.env.PORT || 8000);
 const fs = require("fs");
 
 // Calling CRON jobs.
